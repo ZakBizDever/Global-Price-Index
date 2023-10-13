36da084021d5f7536fbbac1c92188c9363c2cce3
Zakaria JEDDI <zakariajd@Zakarias-MacBook-Pro.local>
2023-10-13 17:45:22 +0100

    Implementation of Global Price Index calculation logic

--- src/routes/globalPriceRoutes.ts
+++ src/routes/globalPriceRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-
-const globalPriceController = require("../controllers/globalPriceController");
+import GlobalPriceController from '../controllers/globalPriceController';
 
 const router = express.Router();
+const globalPriceController = new GlobalPriceController();
 
 //GET
-router.get("/mid-price", globalPriceController.get_mid_price);
+router.get("/mid-price/:exchange/:symbol", globalPriceController.get_mid_price);
 router.get("/:symbol", globalPriceController.get_global_price_index);
 
 module.exports = router;
